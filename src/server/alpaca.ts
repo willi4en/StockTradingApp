08469739b63ba875e395f63aa32c7961b142e6bb
ezra <ezra.furry@gmail.com>
2023-01-23 14:37:04 -0500

    basic api call working!

--- src/server/alpaca.ts
+++
@@ -1,8 +0,0 @@
-import Alpaca from "@alpacahq/alpaca-trade-api";
-import { env } from "../env/server.mjs";
-
-export const alpaca = new Alpaca({
-    keyId: env.API_KEY,
-    secretKey: env.API_SECRET,
-    paper: false,
-});
