cfcdbcefa6628ac4d88107c5408c325991cf5ad3
willi4en <46721116+willi4en@users.noreply.github.com>
2023-01-24 15:29:57 -0500

    Update

--- backend/app.py
+++ backend/app.py
@@ -1,43 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 import datetime
 import yaml
 
-# db = yaml.load(open('sqlconfig.yaml'), Loader=yaml.FullLoader)
+dbconfig = yaml.load(open('sqlconfig.yaml'), Loader=yaml.FullLoader)
 
 app = Flask(__name__)
 
-# app.config['MYSQL_HOST'] = db['mysql_host']
-# app.config['MYSQL_USER'] = db['mysql_user']
-# app.config['MYSQL_PASSWORD'] = db['mysql_password']
-# app.config['MYSQL_DB'] = db['mysql_db']
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = dbconfig['mysql_host']
+app.config['MYSQL_USER'] = dbconfig['mysql_user']
+app.config['MYSQL_PASSWORD'] = dbconfig['mysql_password']
+app.config['MYSQL_DB'] = dbconfig['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://:''@localhost/flask'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
-# mysql = MySQL(app)
-# print(mysql.connection)
+mysql = MySQL(app)
+print(mysql.connection)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    password = db.Column(db.Text())
-    date = db.Column(db.DateTime, default=datetime.datetime.now)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-@app.route('/', methods=['GET'])
-def get_articles():
+@app.route('/')
+def index():
     return jsonify({"Hello": "World"})
 
 
