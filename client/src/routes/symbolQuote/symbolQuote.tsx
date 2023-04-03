f5defe14d8f93f4c4bfcdca8bbe0f9df239f510c
jordan <jordanrwm@gmail.com>
2023-04-03 13:06:26 -0400

    added buy/sell skeleton to quote page

--- client/src/routes/symbolQuote/symbolQuote.tsx
+++ client/src/routes/symbolQuote/symbolQuote.tsx
@@ -30,12 +30,14 @@ function SymbolQuote() {
       <div className="d-flex flex-grow-1">
         <div className="flex-basis-1 d-flex flex-column align-items-center justify-content-center text-end px-5">
           <div className="text-start">
-            <h2>Current price: ${quote.c.toFixed(2)}</h2>
+            <h2>
+              <u>Current price:</u> ${quote.c.toFixed(2)}
+            </h2>
             <h4>Opening price for the day: ${quote.o.toFixed(2)}</h4>
             <h4>High price of the day: ${quote.h.toFixed(2)}</h4>
             <h4>Low price of the day: ${quote.l.toFixed(2)}</h4>
             <h2 className="mt-5">
-              Previous close price: ${quote.pc.toFixed(2)}
+              <u>Previous close price:</u> ${quote.pc.toFixed(2)}
             </h2>
             <h4>
               Change in price (from previous close): ${quote.d.toFixed(2)}
@@ -43,8 +45,29 @@ function SymbolQuote() {
             <h4>Percent change: {quote.dp}%</h4>
           </div>
         </div>
-        <div className="flex-basis-2 d-flex flex-column align-items-center justify-content-center px-5">
+        <div className="flex-basis-2 d-flex flex-column align-items-center justify-content-start px-5 mt-5">
           <CandlestickChart symbol={pathname} />
+          <h5 className="mb-4">
+            Buy/Sell activity for {pathname.toUpperCase()}
+          </h5>
+          <div className="d-flex w-100 justify-content-evenly">
+            <div className="text-center">
+              <h6>
+                <u>Buys</u>
+              </h6>
+              <p>
+                Bought on {} for {}
+              </p>
+            </div>
+            <div className="text-center">
+              <h6>
+                <u>Sells</u>
+              </h6>
+              <p>
+                Sold on {} for {}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
